e03ef3dd6ea4aa7a42c19b60593000ef5579b782
hasanalit <tuxtayevhasan72@gmail.com>
2022-04-03 16:08:57 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Component from './Component';
+
 
 function App() {
   return (
     <div className="App">
      <h1>Hasanali</h1>
+     <Component />
     </div>
   );
 }
